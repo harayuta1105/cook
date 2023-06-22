4b368b5cea0d75b6c413b44e7ef6cad49a90eefb
kin <2220216@ecc.ac.jp>
2023-06-22 11:25:53 +0900

    メニュー作成

--- app/src/main/java/com/example/cooklife/MenuActivity.java
+++ app/src/main/java/com/example/cooklife/MenuActivity.java
@@ -3,12 +3,40 @@ package com.example.cooklife;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class MenuActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
-    }
+        EditText editText;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_menu);
+
+            editText = findViewById(R.id.edit_text);
+        }
+
+        public void button(View view) {
+
+            Calendar calendar = Calendar.getInstance();
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(
+                    this, new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                    editText.setText(String.format(Locale.JAPAN, "%02d / %02d / %02d", year, month + 1, dayOfMonth));
+                }
+            },
+                    calendar.get(Calendar.YEAR),
+                    calendar.get(Calendar.MONTH),
+                    calendar.get(Calendar.DAY_OF_MONTH)
+            );
+            datePickerDialog.show();
+        }
 }
